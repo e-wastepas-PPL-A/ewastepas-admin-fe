f87d1c4020bbf72a7e2e20b68e770bf3679addc2
Kinn-01 <gilmanarief23@gmail.com>
2024-10-13 14:47:26 +0700

    Setup Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+
+function App() {
+  
+  return (
+    <h1 className="text-3xl font-bold underline flex justify-center items-center min-h-screen">
+    Hello world!
+    </h1>
+  )
+}
+
+export default App
