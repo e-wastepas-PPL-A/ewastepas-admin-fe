15db30f6fe356cbeb5e9d0d05ccec8b4d7a45e99
Kinn-01 <gilmanarief23@gmail.com>
2024-10-21 23:36:32 +0700

    Add Dashboard & Register Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
+import Dashboard from './pages/DashboardPage.jsx'
+import Register from './pages/RegisterPage.jsx'
 import './style/index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/test",
+    element: <App />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
