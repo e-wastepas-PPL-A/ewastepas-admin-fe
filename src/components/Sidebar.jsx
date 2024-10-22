fae08b86a6290685437d399a08ae8406147b2a6b
yaelahbarr <albarrapermana027@gmail.com>
2024-10-22 18:06:16 +0700

    pembaharuan navbar, sidebar, dan penambahan halaman Permintaan Sampah serta tabel nya

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,24 +2,33 @@
 "use client";
 
 import { Sidebar } from "flowbite-react";
-import { HiArchive, HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+import { HiArchive, HiArrowSmRight, HiChartPie, HiInbox, HiReceiptRefund, HiShoppingBag, HiTable, HiTrash, HiTruck, HiUser, HiUserGroup, HiViewBoards } from "react-icons/hi";
+
+const customTheme = {
+  root: {
+    inner: 'h-full overflow-y-auto overflow-x-hidden bg-white-1000 px-3 py-4'
+  }
+}
 
 export default function CustomSidebar() {
   return (
-    <Sidebar aria-label="Sidebar with logo">
-      {/* <Sidebar.Logo href="#" img="/favicon.svg" imgAlt="">
-        E-Whale
-      </Sidebar.Logo> */}
+    <Sidebar aria-label="Sidebar with logo" className="bg-white" theme={customTheme}>
+      <Sidebar.Logo href="#" img="/dummy-logo.png" imgAlt="">
+        E-WHALE
+      </Sidebar.Logo>
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiChartPie}>
-            Dashboard
+          <Sidebar.Item href="#" icon={HiUser}>
+            Approval Registrasi
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiViewBoards}>
-            Penjemputan Sampah
+          <Sidebar.Item href="#" icon={HiTruck}>
+            Registrasi Kurir
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiArchive}>
-            History
+          <Sidebar.Item href="#" icon={HiUserGroup}>
+            Registrasi Masyarakat
+          </Sidebar.Item>
+          <Sidebar.Item href="/permintaan-sampah" icon={HiTrash}>
+            Permintaan Sampah
           </Sidebar.Item>
         </Sidebar.ItemGroup>
       </Sidebar.Items>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Dashboard from './pages/DashboardPage.jsx'
+import Register from './pages/RegisterPage.jsx'
+import PermintaanSampah from './pages/PermintaanSampah.jsx'
 import './style/index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/test",
+    element: <App />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/permintaan-sampah",
+    element: <PermintaanSampah />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
