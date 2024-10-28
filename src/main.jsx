899467a0c9bd68039e1bf379cc7ead83df4f3808
Kinn-01 <gilmanarief23@gmail.com>
2024-10-28 15:59:28 +0700

    Slicing UI Halaman Registrasi Kurir, Jenis & Kategori, Konversi Poin, Dropbox

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import Dashboard from './pages/DashboardPage.jsx'
 import Register from './pages/RegisterPage.jsx'
+import RegisterKurir from './pages/RegisterKurirPage.jsx'
 import PermintaanSampah from './pages/PermintaanSampah.jsx'
+import KonversiPoin from './pages/KonversiPoin.jsx'
+import JenisKategori from './pages/JenisKategoriPage.jsx'
+import Dropbox from './pages/DropboxPage.jsx'
 import './style/index.css'
 
 const router = createBrowserRouter([
@@ -20,9 +24,25 @@ const router = createBrowserRouter([
     element: <Register />,
   },
   {
+    path: "/register-kurir",
+    element: <RegisterKurir />,
+  },
+  {
     path: "/permintaan-sampah",
     element: <PermintaanSampah />,
   },
+  {
+    path: "/konversi-poin",
+    element: <KonversiPoin />,
+  },
+  {
+    path: "/jenis-kategori",
+    element: <JenisKategori />,
+  },
+  {
+    path: "/dropbox",
+    element: <Dropbox />,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
